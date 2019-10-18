96c131f0d9ed048d4e2b3d983f2331b58575dbb5
louis.liao <buzonliao@gmail.com>
2019-10-18 15:00:38 +0800

    first commit

---
+++ Minted/app/src/main/java/com/example/louis/minted/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.louis.minted;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
